29a4e658a1d9438d12fe08a41e5fb610cac77e91
Eric Golde <eric@golde.org>
2018-01-15 21:25:27 -0800

    Resetting token cause Im dumb

--- src/org/golde/discord/pumpbot/BotConstants.java
+++ src/org/golde/discord/pumpbot/BotConstants.java
@@ -2,6 +2,6 @@ package org.golde.discord.pumpbot;
 
 public class BotConstants {
 
-	public static final String TOKEN = "NDAyNjc3Mjg0NTU0ODAxMTUz.DT8N_A.YgT3cTmem8Cy4UOczcnWOYZ5vZM";
+	public static final String TOKEN = "*****";
 	
 }
